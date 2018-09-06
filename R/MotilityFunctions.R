07809dbad9d42ea9fd0845d4333baaa1c5848ca9
sebastiz <sebastiz@hotmail.com>
2018-09-06 10:00:40 +0000

    Further neatening up of symptoms

--- R/MotilityFunctions.R
+++ R/MotilityFunctions.R
@@ -175,6 +175,27 @@ ifelse(data$ResidualmeanmmHg<15&data$failedChicagoClassification>=30&data$failed
   return(data)
 }
 
+##### Merging with another table ####
+
+#' Merge HRM and Impedance
+#' This merges the overall impedance dataset with the HRM dataset
+#' @param dataImpWhole the whole impedance dataset
+#' @param dataHRM the whole HRM dataset
+#' @keywords HRMAndSwallows acquisition
+#' @export
+#' @examples
+#' #dataHRM<-dataHRM(channel)
+#' #dataImp2<-dataImp2(channel)
+#' #dataImp_Symp<-dataImp_Symp(channel)
+#'
+#' #dataImpWhole<-dataImpWhole(dataImp2,dataImp_Symp)
+#' #MyImpedanceDataWithHRM(dataImpWhole,dataHRM)
+
+MyImpedanceDataWithHRM<-function(dataImpWhole,dataHRM){
+  MyImpedanceDataWithHRM<-merge(dataImpWhole,dataHRM,by=c("HospNum_Id"),all=TRUE)
+  return(MyImpedanceDataWithHRM)
+}
+
 
 ########################### Some metrics #################################################################################
 #' MotilityTimeSeries
