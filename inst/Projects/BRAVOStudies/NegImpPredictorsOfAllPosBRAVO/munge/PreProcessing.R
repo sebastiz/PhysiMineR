050b8b742e6e8c3f66308cab07e42a0b6caaa620
sebastiz <sebastiz@hotmail.com>
2019-05-08 17:09:33 +0000

    PhysiMineR Project Neg Impedance project improved

---
+++ inst/Projects/BRAVOStudies/NegImpPredictorsOfAllPosBRAVO/munge/PreProcessing.R
@@ -0,0 +1,293 @@
+
+
+library(dplyr)
+library(stringr)
+library(here)
+library(EndoMineR)
+library(DiagrammeR)
+library(readxl)
+library(tidyr)
+library(PhysiMineR)
+library(DataExplorer)
+library(psych)
+
+##### Import ####
+## @knitr dataImport
+
+oldw <- getOption("warn")
+options(warn = -1)
+
+#Get this from the data folder for PhysiMineR-  just click on the RData to load into
+#the global environment
+
+#Or if using the local file system
+
+BravoDayOneAndTwo <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/BravoDay1And2.xls"))
+BravoDayThreeAndFour <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/BravoDay3And4.xls"))
+BRAVOTotal <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/BRAVOTotal.xls"))
+Diag <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/Diag.xlsx"))
+HRMImportMain <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/HRMImportMain.xls"))
+HRMImportSwallows <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/HRMImportSwallows.xls"))
+Imp_Symp <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/Imp_Symp.xls"))
+ImpedanceTwo <- read_excel(here::here("inst/Projects/BRAVOStudies/HRMPredictorsOfAllPosBRAVO/data/Impedance2.xls"))
+
+options(warn = oldw)
+
+
+
+
+##### Clean ####################################################################################################################
+## @knitr dataClean
+#Data cleaning
+#Use after PhysiData_Acq
+
+#Clean HRM
+#Clean up the HRM swallows:
+HRMImportSwallows<-HRMImportSwallows[!is.na(HRMImportSwallows$panesophagealpressurizationMapSwallowsNum8),]
+
+#Clean Diag
+Diag$HospNum_Id<-toupper(Diag$HospNum_Id)
+Diag$FileCreationDate<-as.Date(gsub("(\\d{4}-\\d{2}-\\d{2}).*","\\1",Diag$FileCreationDate))
+#Convert VisitDate into a Date format and extract the year (the month and day are unreliable here as sometimes American and sometimes British dates are used)
+
+#Convert the dates to date format
+Diag$VisitDate2<-as.Date(Diag$VisitDate,format="%d_%m_%Y")
+#For NA dates make sure it is not because the dates are the wrong way around
+Diag$VisitDate3<-as.Date(Diag$VisitDate,format="%m_%d_%Y")
+
+#Merge the dates if there are separate HRM and Diag results which are reporting the same thing
+Diag$VisitDate4<-as.Date(ifelse(is.na(Diag$VisitDate2),Diag$VisitDate3,Diag$VisitDate2),origin = "1970-01-01")
+#If still NA then use the file creation date as the visit date
+Diag$VisitDate4<-as.Date(ifelse(is.na(Diag$VisitDate4),Diag$FileCreationDate,Diag$VisitDate4),origin = "1970-01-01")
+
+#Extract the Distal LES as is likely to be in both the final and the HRM report so can be merged on this
+Diag$DistalLESnares<-gsub(".*Distal LES from nares.*?(\\d+).*","\\1",Diag$WholeReport)
+#Get rid of whole reports that are copied over for some reason
+Diag$DistalLESnares<-gsub(".*[A-Za-z].*","\\1",Diag$DistalLESnares)
+
+
+
+
+
+
+
+##### Merge ########################################################################################
+## @knitr dataMerge
+
+
+
+AllImpedance<-merge(ImpedanceTwo,Imp_Symp,by="Imp_Id", all = TRUE)
+
+#Merge the HRM results together (swallows and Main)
+AllHRM<-merge(HRMImportMain,HRMImportSwallows,by="HRM_Id",all=TRUE)
+
+
+AllBravo<-merge(BravoDayOneAndTwo,BravoDayThreeAndFour,by="BravoID",all=TRUE)
+AllBravo<-merge(AllBravo,BRAVOTotal,by="BravoID",all=TRUE)
+AllBravo<-dataBRAVOClean(AllBravo)
+AllBravo<-dataBRAVODayLabeller(AllBravo,"HospNum_Id","VisitDate")
+
+
+
+
+
+
+
+
+
+##### Clean2 ############################################################################################################
+## @knitr dataClean2
+
+#Remove duplicate reports on the basis of being the same Hospital Number and date range here
+#Also need to get rid of the duplicates from the Diag table:
+
+
+DiagTwo<-Diag %>%
+  arrange(DistalLESnares,HospNum_Id) %>%
+  group_by(DistalLESnares,HospNum_Id,lubridate::year(VisitDate4)) %>%
+  summarise_all(.funs = function(x) paste(unique(c(dplyr::lag(x, default = NULL), x)), collapse = ":"))
+
+
+#Clean up the HRM HRMImportMain (to get rid of duplicates mainly)
+#This looks for all the duplicates by hospital number and DistalLESfromnarescm (the latter so that duplicates means same test rather than same patient on two different days)
+
+HRMImportMainTwo<-AllHRM %>%
+  group_by(HospNum_Id,DistalLESfromnarescm) %>%
+  summarise_all(.funs = function(x) paste(unique(c(dplyr::lag(x, default = NULL), x)), collapse = ":"))
+
+#Clean Up the main HRM
+HRMImportMainTwo<-HRMCleanUp1(HRMImportMainTwo)
+#Not sure why I did this.
+HRMImportMainTwo$LOS_relax<-as.factor(HRMImportMainTwo$LOS_relax)
+HRMImportMainTwo$LowerOesoph<-as.factor(HRMImportMainTwo$LowerOesoph)
+
+
+# Get the whole impedance dataset
+#You will need to re-clean the merged dataImpSympImpedance as cleaning function needed to be fixed
+ImpedanceThree<-dataImpClean(AllImpedance)
+#Extract all the symptoms
+ImpAll<-dataImpSymptoms(ImpedanceThree)
+
+
+
+
+#Merge all HRM with AllBravo to get the full dataset
+#This merges the tests so that the nearest hrm to the BRAVO is chosen to avoid replication
+#Note that a lot of Bravo's do not have HRM as may not have been requested by outside referrers
+BravoAndHRM <- HRMImportMainTwo %>% inner_join(AllBravo, by ="HospNum_Id") %>%
+  mutate(Date_ABS_Diff = abs(VisitDate.x - VisitDate.y)) %>%
+  arrange(HospNum_Id, Date_ABS_Diff) %>%
+  group_by(HospNum_Id) %>%
+  slice(1)
+
+
+
+#Need to change this so that is part of the PhysiMineR codebase (when I can get AWS to stop crashing of course)
+BravoAndHRM<-GORD_AcidBRAVO(BravoAndHRM)
+
+
+
+ImpAndHRM <- BravoAndHRM %>% inner_join(ImpAll, by ="HospNum_Id") %>%
+  group_by(HospNum_Id) %>%
+  slice(1)
+
+
+
+
+##### Extrapolate ############################################################################################################
+## @knitr dataExtrapolate
+#Extract and clean the meds (need to bind to the existing dataframe)
+#Run after PhysiData_Acq
+#Run after PhysiData_Clean
+
+
+#Extract some diagnoses from the report (using the histopath extractor from EndoMineR)
+DiagTwo$Dx<-str_extract_all(DiagTwo$WholeReport, paste0("upragas|neffecti|ackham|utcrack|[Aa]peris|[Ff]requent [Ff]ail" , simplify = FALSE))
+DiagTwo$Dx <- sapply(DiagTwo$Dx, toString)
+
+
+
+
+
+#2.Recoding
+BravoAndHRM$AcidRefluxBRAVO<-gsub("NoAcid",0,BravoAndHRM$AcidRefluxBRAVO)
+BravoAndHRM$AcidRefluxBRAVO<-gsub(".*Acid",1,BravoAndHRM$AcidRefluxBRAVO)
+BravoAndHRM$Hiatalhernia<-ifelse(BravoAndHRM$Hiatalhernia=="Yes",1,0)
+BravoAndHRM$AcidRefluxBRAVO<-as.numeric(BravoAndHRM$AcidRefluxBRAVO)
+
+
+
+
+##### Data exploration #################################################################################
+## @knitr DataExploration
+
+#1. Get rid of bad columns with missing data
+#Tells you which columns have the most missing data so they can be excluded
+missing<-profile_missing(BravoAndHRM)
+missing<-data.frame(missing,stringsAsFactors = FALSE)
+#Only <10% for each row missing data is accepted
+BravoAndHRMNoMissing<-missing[!grepl("Bad|Remove",missing$group),]
+#Pick the columns with good data in them from this set
+BravoAndHRMNoMissing<-BravoAndHRM[,match(as.character(BravoAndHRMNoMissing$feature),names(BravoAndHRM))]
+#3. Get some basic descriptive stats:
+rr<-describe(BravoAndHRMNoMissing)
+
+
+
+
+#
+#
+#
+# ###################### Univariate Anaylsis ########################################
+# ## @knitr HRM_UnivariateAnalysis
+# #Univariate analysis:
+# NegImpPosBRAVOFinal<-BravoAndHRMNoMissing[BravoAndHRMNoMissing$AcidRefluxBRAVO==1,]
+# NegImpNegBRAVOFinal<-BravoAndHRMNoMissing[BravoAndHRMNoMissing$AcidRefluxBRAVO==0,]
+#
+# #Univariate analysis
+# t.test(NegImpPosBRAVOFinal$BasalrespiratoryminmmHg ,NegImpNegBRAVOFinal$BasalrespiratoryminmmHg,paired=FALSE)
+# t.test(NegImpPosBRAVOFinal$prematurecontraction,NegImpNegBRAVOFinal$prematurecontraction,paired=FALSE)
+# t.test(NegImpPosBRAVOFinal$Distallatency,NegImpNegBRAVOFinal$Distallatency,paired=FALSE)
+# t.test(NegImpPosBRAVOFinal$largebreaks,NegImpNegBRAVOFinal$largebreaks,paired=FALSE)
+#
+#
+#
+#
+#
+#
+#
+# ##### Variable Selection #############################################
+#
+# ## @knitr VariableSelection
+#
+# #Variable selection- choose HRM variables only here. Got rid of BasalrespiratorymeanmmHg,ContractileFrontVelocity,HiatusHernia and most oesophageal length measurements because of colinearity concerns with other variables
+#
+# BravoAndHRMNoMissingFinal<-BravoAndHRMNoMissing %>%
+#   ungroup()%>%
+#   select(IntraabdominalLESlengthcm,
+#          Hiatalhernia,
+#          LOS_relax,
+#          BasalrespiratoryminmmHg,
+#          ResidualmeanmmHg,
+#          DistalcontractileintegralmeanmmHgcms,
+#          IntraboluspressureATLESRmmHg,
+#          Distallatency,
+#          failedChicagoClassification,
+#          panesophagealpressurization,
+#          largebreaks,
+#          prematurecontraction,
+#          rapidcontraction,
+#          smallbreaks,
+#          AcidRefluxBRAVO)
+#
+# #Check for multicolinearity
+# library("PerformanceAnalytics")
+# chart.Correlation(BravoAndHRMNoMissingFinal, histogram=TRUE, pch=1)
+# #Exploratory data analysis:
+# create_report(BravoAndHRMNoMissingFinal)
+#
+# #Consider changing variables and repeat step 4
+#
+# ########## Multiple Logistic Regression#############################################
+# ## @knitr HRM_MultipleLogRegression
+#
+# library(MASS)
+# # Fit the full model
+# data<-na.omit(NegImpALL_BRAVO_WithImpAndHRMNoMissingFinal)
+# full.model <- glm(AcidRefluxBRAVO ~ ., data = data)
+# # Stepwise regression model
+# step.model <- stepAIC(full.model, direction = "both",
+#                       trace = FALSE)
+#
+# summary(step.model)
+#
+# library(pROC)
+# predpr <- predict(full.model,type=c("response"))
+# roccurve <- roc(AcidRefluxBRAVO ~ predpr, data = data)
+# plot(roccurve)
+# auc(roccurve)
+#
+#
+#
+# #Get another ROC curve
+# library(ROCR)
+# prediction( NegImpALL_BRAVO_WithImpNoMissing$MainAcidExpUprightClearanceChannelLongestEpisode, NegImpALL_BRAVO_WithImpNoMissing$AcidRefluxBRAVO)
+#
+# perf1 <- performance(pred, "sens", "spec")
+# plot(perf1)
+#
+# library(OptimalCutpoints)
+# NegImpALL_BRAVO_WithImpNoMissing<-data.frame(NegImpALL_BRAVO_WithImpNoMissing,stringsAsFactors = F)
+# optimal.cutpoints(X="MainAcidCompositeScorePatientValueUprightTimeInReflux",status="AcidRefluxBRAVO",tag.healthy=1,methods = "Youden",data=NegImpALL_BRAVO_WithImpNoMissing, pop.prev = NULL,
+#                   control = control.cutpoints(), ci.fit = FALSE, conf.level = 0.95, trace = FALSE)
+#
+#
+#
+#
+#
+#
+# detach("package:MASS", unload=TRUE)
+#
+#
+#
+#
